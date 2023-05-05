dc504a14666f6a776f92c131e63f50b5bbbe533e
RobertaFreitas <robertaefreitas@gmail.com>
2023-05-05 23:53:49 +0100

    add event listener to keydown event

--- index.js
+++ index.js
@@ -5,34 +5,43 @@ function playSound(sound) {
   audio.play();
 }
 
+function handleDrumClick(buttonInnerHTML) {
+  switch (buttonInnerHTML) {
+    case "w":
+      playSound('sounds/tom-1.mp3');
+      break;
+    case "a":
+      playSound('sounds/tom-2.mp3');
+      break;
+    case "s":
+      playSound('sounds/tom-3.mp3');
+      break;
+    case "d":
+      playSound('sounds/tom-4.mp3');
+      break;
+    case "j":
+      playSound('sounds/snare.mp3');
+      break;
+    case "k":
+      playSound('sounds/crash.mp3');
+      break;
+    case "l":
+      playSound('sounds/kick-bass.mp3');
+      break;
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+function handleKeyDown(event) {
+  handleDrumClick(event.key);
+}
+
 drums.forEach((drum) => {
   drum.addEventListener('click', function() {
     var buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        playSound('sounds/tom-1.mp3');
-        break;
-      case "a":
-        playSound('sounds/tom-2.mp3');
-        break;
-      case "s":
-        playSound('sounds/tom-3.mp3');
-        break;
-      case "d":
-        playSound('sounds/tom-4.mp3');
-        break;
-      case "j":
-        playSound('sounds/snare.mp3');
-        break;
-      case "k":
-        playSound('sounds/crash.mp3');
-        break;
-      case "l":
-        playSound('sounds/kick-bass.mp3');
-        break;
-      default:
-        console.log('buttonInnerHTML');
-    }
+    handleDrumClick(buttonInnerHTML);
   });
 });
+
+document.addEventListener('keydown', handleKeyDown);
